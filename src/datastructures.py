3f69074a2b1b9dff13c5276368d343c949245a1a
Armando <164431619+Armando7101@users.noreply.github.com>
2025-05-26 15:24:39 +0000

    listoo

--- src/datastructures.py
+++ src/datastructures.py
@@ -6,45 +6,63 @@ class FamilyStructure:
 
         self._members = [
             {
-                "id": self._generateId(),
-                "first_name": "Jhon",
-                "last_name": last_name,
+                "id":3443,
+                "first_name":"Tommy",
+                "last_name":self.last_name,
                 "age": 33,
-                "lucky_numbers":[7, 13, 22]
+                "lucky_numbers":[7,13,22]
+
             },
             {
-                "id": self._generateId(),
-                "first_name": "Jane",
-                "last_name": last_name,
+                "id":self._generateId(),
+                "first_name":"Jane",
+                "last_name":self.last_name,
                 "age": 35,
-                "lucky_numbers":[10, 14, 3]
+                "lucky_numbers":[10,14,3]
+
             },
             {
-                "id": self._generateId(),
-                "first_name": "Jimmy",
-                "last_name": last_name,
+                "id":self._generateId(),
+                "first_name":"Jimmy",
+                "last_name":self.last_name,
                 "age": 5,
                 "lucky_numbers":[1]
+
             }
+
+
+
+
         ]
 
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        self._members.append(member)
-        return None
+       new_member={
+                "id":member.get("id",self._generateId()),
+                "first_name":member.get("first_name"),
+                "last_name":self.last_name,
+                "age": member.get("age"),
+                "lucky_numbers":member.get("lucky_numbers")
+
+            }
+       self._members.append(new_member)
+       return new_member
 
     def delete_member(self, id):
-        for position in range(len(self._members)):
-            if self._members[position]["id"] == id:
-                self._members.pop(position)
-                return None
-
+      for index in range (0,len(self._members)):
+          if self._members[index].get("id")==id:
+               self._members.remove(self._members[index])
+               return {"done":True}
+      return None
+    
     def get_member(self, id):
-        for member in self._members:
-            if member ["id"] == id:
-                return member
-                
+      for member in self._members:
+        if member.get("id")==id:
+           return member
+      return None
+         
+
     def get_all_members(self):
         return self._members
